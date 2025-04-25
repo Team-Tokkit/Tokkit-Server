c1b06c0784952f723bfc9bedf5d1700b73f02e08
YoungjaeRo <ryj0330@naver.com>
2025-04-25 17:28:19 +0900

    feat : 전자지갑 Entity 구현

---
+++ src/main/java/com/example/Tokkit_server/domain/Wallet.java
@@ -0,0 +1,40 @@
+package com.example.Tokkit_server.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Wallet {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Column(nullable = false)
+	private Long depositBalance; // 예금 잔액
+
+	private Long tokenBalance; // 토큰 잔액
+
+	public void updateBalance(Long deposit, Long token) {
+		this.depositBalance = deposit;
+		this.tokenBalance = token;
+	}
+}
