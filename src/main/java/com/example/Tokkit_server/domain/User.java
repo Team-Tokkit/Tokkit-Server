8b0e086b4c032d3bdd4fe83983c8e7355d8db4da
YoungjaeRo <ryj0330@naver.com>
2025-04-28 12:24:48 +0900

    refactor : User 필드 (이름) 추가

--- src/main/java/com/example/Tokkit_server/domain/User.java
+++ src/main/java/com/example/Tokkit_server/domain/User.java
@@ -1,10 +1,13 @@
 package com.example.Tokkit_server.domain;
 
-import jakarta.persistence.Column;
+import com.example.Tokkit_server.domain.common.BaseTimeEntity;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,20 +19,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class User {
+public class User extends BaseTimeEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
-    private String phoneNumber;
-
-    @Column(nullable = false)
     private String email;
 
-    @Column(nullable = true)
-    private String nickname;
+    private String password;
 
+    private String phoneNumber;
+
+    private Boolean isDormant; // 휴면 계정 여부
+
+    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
+    private Wallet wallet;
 
 }
