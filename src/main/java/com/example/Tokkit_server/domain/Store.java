142eeafdca0c305f80c9838708cd21334b2b735d
YoungjaeRo <ryj0330@naver.com>
2025-04-28 15:58:49 +0900

    feat : 새로운 entity 구현

---
+++ src/main/java/com/example/Tokkit_server/domain/Store.java
@@ -0,0 +1,46 @@
+package com.example.Tokkit_server.domain;
+
+import com.example.Tokkit_server.Enum.StoreCategory;
+import com.example.Tokkit_server.domain.common.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Store extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private StoreCategory storeCategory;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "merchant_id")
+	private Merchant merchant;
+
+	@Column(nullable = false)
+	private String adress;
+
+}
