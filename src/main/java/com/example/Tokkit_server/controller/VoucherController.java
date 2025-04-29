660aaa660122cb94b7c16869f45d67a66e5d9e54
jeongmin07262 <dlwjdals0726@gmail.com>
2025-04-29 14:09:46 +0900

    feat: 바우처 상세 조회 기능 구현

--- src/main/java/com/example/Tokkit_server/controller/VoucherController.java
+++ src/main/java/com/example/Tokkit_server/controller/VoucherController.java
@@ -1,11 +1,15 @@
 package com.example.Tokkit_server.controller;
 
 import com.example.Tokkit_server.apiPayload.ApiResponse;
+import com.example.Tokkit_server.dto.response.StoreResponse;
+import com.example.Tokkit_server.dto.response.VoucherDetailResponse;
 import com.example.Tokkit_server.dto.response.VoucherResponse;
 import com.example.Tokkit_server.service.query.VoucherQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.domain.Pageable;
@@ -24,5 +28,21 @@ public class VoucherController {
         Page<VoucherResponse> vouchers = voucherQueryService.getAllVouchers(pageable);
         return ApiResponse.onSuccess(vouchers);
     }
+
+    // 바우처 상세 조회하기
+    @GetMapping("/details/{id}")
+    public ApiResponse<VoucherDetailResponse> getVoucherDetail(@PathVariable Long id) {
+        Pageable pageable = PageRequest.of(0, 5);
+        VoucherDetailResponse voucherDetail = voucherQueryService.getVoucherDetail(id, pageable);
+        return ApiResponse.onSuccess(voucherDetail);
+    }
+
+    // 바우처 상세 조회 (사용처 전체 조회)
+    @GetMapping("/details/{id}/stores")
+    public ApiResponse<Page<StoreResponse>> getAllStoresByVoucherId(@PathVariable Long id, Pageable pageable) {
+        Page<StoreResponse> stores = voucherQueryService.getAllStoresByVoucherId(id, pageable);
+        return ApiResponse.onSuccess(stores);
+    }
+
 
 }
--- src/main/java/com/example/Tokkit_server/service/query/VoucherQueryServiceImpl.java
+++ src/main/java/com/example/Tokkit_server/service/query/VoucherQueryServiceImpl.java
@@ -1,6 +1,12 @@
 package com.example.Tokkit_server.service.query;
 
+import com.example.Tokkit_server.apiPayload.code.status.ErrorStatus;
+import com.example.Tokkit_server.apiPayload.exception.GeneralException;
+import com.example.Tokkit_server.domain.Voucher;
+import com.example.Tokkit_server.dto.response.StoreResponse;
+import com.example.Tokkit_server.dto.response.VoucherDetailResponse;
 import com.example.Tokkit_server.dto.response.VoucherResponse;
+import com.example.Tokkit_server.repository.StoreRepository;
 import com.example.Tokkit_server.repository.VoucherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -12,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class VoucherQueryServiceImpl implements VoucherQueryService {
 
     private final VoucherRepository voucherRepository;
+    private final StoreRepository storeRepository;
 
     // 전체 바우처 조회하기
     @Override
@@ -19,5 +26,25 @@ public class VoucherQueryServiceImpl implements VoucherQueryService {
         return voucherRepository.findAll(pageable)
                 .map(VoucherResponse::from);
     }
+
+    // 바우처 상세 조회 (상위 5개의 사용처 조회)
+    @Override
+    public VoucherDetailResponse getVoucherDetail(Long id, Pageable pageable) {
+        Voucher voucher = voucherRepository.findById(id)
+                .orElseThrow(() -> new GeneralException(ErrorStatus.VOUCHER_NOT_FOUND));
+
+        Page<StoreResponse> stores = storeRepository.findByVouchersId(id, pageable)
+                .map(StoreResponse::from);
+
+        return VoucherDetailResponse.from(voucher, stores);
+    }
+
+    // 바우처 상세 조회 (사용처 전체 조회)
+    @Override
+    public Page<StoreResponse> getAllStoresByVoucherId(Long id, Pageable pageable) {
+        return storeRepository.findByVouchersId(id, pageable)
+                .map(StoreResponse::from);
+    }
+
 
 }
