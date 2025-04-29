febc48924fe7f9c9571f88c7ca41ab647c99f658
jeongmin07262 <dlwjdals0726@gmail.com>
2025-04-29 16:31:35 +0900

    feat: VoucherOwnershipRepository 구현

--- src/main/java/com/example/Tokkit_server/repository/VoucherOwnershipRepository.java
+++ src/main/java/com/example/Tokkit_server/repository/VoucherOwnershipRepository.java
@@ -1,10 +1,14 @@
 package com.example.Tokkit_server.repository;
 
 import com.example.Tokkit_server.domain.VoucherOwnership;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface VoucherOwnershipRepository extends JpaRepository<VoucherOwnership, Long> {
-    List<VoucherOwnership> findByUserId(Long userId);
+
+    // 특정 유저가 소유한 바우처 조회
+    Page<VoucherOwnership> findByWalletUserId(Long userId, Pageable pageable);
 }
