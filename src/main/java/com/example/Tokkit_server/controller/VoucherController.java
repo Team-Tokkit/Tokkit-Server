0d5fdb31792b4754bf1f0a2976441ce3c4d7c7d0
jeongmin07262 <dlwjdals0726@gmail.com>
2025-05-01 09:24:57 +0900

    feat: 스웨거 적용

--- src/main/java/com/example/Tokkit_server/controller/VoucherController.java
+++ src/main/java/com/example/Tokkit_server/controller/VoucherController.java
@@ -6,6 +6,8 @@ import com.example.Tokkit_server.dto.response.StoreResponse;
 import com.example.Tokkit_server.dto.response.VoucherDetailResponse;
 import com.example.Tokkit_server.dto.response.VoucherResponse;
 import com.example.Tokkit_server.service.query.VoucherQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,19 +18,22 @@ import org.springframework.data.domain.Pageable;
 @RestController
 @RequestMapping("/api/vouchers")
 @RequiredArgsConstructor
+@Tag(name = "Vouchers", description = "바우처 관련 API")
 public class VoucherController {
 
     private final VoucherQueryService voucherQueryService;
 
     // 전체 바우처 조회하기 , 필터링 및 검색하기
     @GetMapping
+    @Operation(summary = "전체 바우처 조회 및 필터링/검색하기", description = "공지사항 전체 목록을 조회하고 필터링 및 검색할 수 있는 API입니다.")
     public ApiResponse<Page<VoucherResponse>> getVouchers(@ModelAttribute VoucherSearchRequest request, Pageable pageable) {
         Page<VoucherResponse> result = voucherQueryService.searchVouchers(request, pageable);
         return ApiResponse.onSuccess(result);
     }
 
-    // 바우처 상세 조회하기
+    // 바우처 상세 조회하기 (사용처 5개 조회)
     @GetMapping("/details/{id}")
+    @Operation(summary = "바우처 상세 조회하기 (상위 5개 사용처 조회)", description = "바우처의 상세 정보를 조회하는 API입니다.")
     public ApiResponse<VoucherDetailResponse> getVoucherDetail(@PathVariable Long id) {
         Pageable pageable = PageRequest.of(0, 5);
         VoucherDetailResponse voucherDetail = voucherQueryService.getVoucherDetail(id, pageable);
@@ -37,6 +42,7 @@ public class VoucherController {
 
     // 바우처 상세 조회 (사용처 전체 조회)
     @GetMapping("/details/{id}/stores")
+    @Operation(summary = "바우처 상세 조회 (사용처 전체 조회)", description = "바우처의 사용처를 전체 조회하는 API입니다.")
     public ApiResponse<Page<StoreResponse>> getAllStoresByVoucherId(@PathVariable Long id, Pageable pageable) {
         Page<StoreResponse> stores = voucherQueryService.getAllStoresByVoucherId(id, pageable);
         return ApiResponse.onSuccess(stores);
--- src/main/java/com/example/Tokkit_server/controller/VoucherOwnershipController.java
+++ src/main/java/com/example/Tokkit_server/controller/VoucherOwnershipController.java
@@ -5,6 +5,8 @@ import com.example.Tokkit_server.dto.response.StoreResponse;
 import com.example.Tokkit_server.dto.response.VoucherOwnershipDetailResponse;
 import com.example.Tokkit_server.dto.response.VoucherOwnershipResponse;
 import com.example.Tokkit_server.service.query.VoucherOwnershipQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,12 +16,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/my-vouchers")
 @RequiredArgsConstructor
+@Tag(name = "MyVouchers", description = "내 바우처 관련 API")
 public class VoucherOwnershipController {
 
     private final VoucherOwnershipQueryService voucherOwnershipQueryService;
 
     // 내 바우처 조회하기
     @GetMapping
+    @Operation(summary = "내 바우처 조회하기", description = "내가 보유한 바우처 목록을 조회하는 API입니다.")
     public ApiResponse<Page<VoucherOwnershipResponse>> getMyVouchers(@RequestParam Long userId, Pageable pageable) {
         Page<VoucherOwnershipResponse> myVouchers = voucherOwnershipQueryService.getMyVouchers(userId, pageable);
         return ApiResponse.onSuccess(myVouchers);
@@ -27,6 +31,7 @@ public class VoucherOwnershipController {
 
     // 내 바우처 상세 조회하기 - 상위 5개의 사용처 조회 (default)
     @GetMapping("/details/{voucherOwnershipId}")
+    @Operation(summary = "내 바우처 상세 조회하기 (상위 5개 사용처 조회)", description = "내가 보유한 바우처의 상세 정보를 조회하는 API입니다.")
     public ApiResponse<VoucherOwnershipDetailResponse> getVoucherDetails(@PathVariable Long voucherOwnershipId, @RequestParam Long userId) {
         Pageable pageable = PageRequest.of(0, 5);
         VoucherOwnershipDetailResponse voucherDetail = voucherOwnershipQueryService.getVoucherDetail(voucherOwnershipId, userId, pageable);
@@ -35,6 +40,7 @@ public class VoucherOwnershipController {
 
     // 내 바우처 상세 조회하기 - 사용처 전체 조회
     @GetMapping("/details/{voucherOwnershipId}/stores")
+    @Operation(summary = "내 바우처 상세 조회하기 (사용처 전체 조회)", description = "내가 보유한 바우처의 사용처를 전체 조회하는 API입니다.")
     public ApiResponse<Page<StoreResponse>> getAllStoresByVoucherId(@PathVariable Long voucherOwnershipId, @RequestParam Long userId, Pageable pageable) {
         Page<StoreResponse> stores = voucherOwnershipQueryService.getAllStoresByVoucherId(voucherOwnershipId, userId, pageable);
         return ApiResponse.onSuccess(stores);
