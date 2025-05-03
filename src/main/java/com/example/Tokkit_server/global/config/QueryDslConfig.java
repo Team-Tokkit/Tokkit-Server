35ceba3b035d561df6f6077a206cf347da64f7b4
sengjun0624 <sengjun0624@naver.com>
2025-05-03 15:37:10 +0900

    chore: 기타 파일 변경 및 삭제

--- src/main/java/com/example/Tokkit_server/global/config/QueryDslConfig.java
+++ src/main/java/com/example/Tokkit_server/global/config/QueryDslConfig.java
@@ -1,4 +1,4 @@
-package com.example.Tokkit_server.config;
+package com.example.Tokkit_server.global.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
