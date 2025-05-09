ab55f24f54dd24c1ad13bd4ba765395afd084432
YoungjaeRo <ryj0330@naver.com>
2025-05-09 10:59:01 +0900

    refactor : StoreCategory  엔터티, 파생 클래스들 삭제

--- src/main/java/com/example/Tokkit_server/global/repository/StoreCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Tokkit_server.global.repository;
-
-import com.example.Tokkit_server.global.entity.StoreCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreCategoryRepository extends JpaRepository<StoreCategory, Long> {
-}
