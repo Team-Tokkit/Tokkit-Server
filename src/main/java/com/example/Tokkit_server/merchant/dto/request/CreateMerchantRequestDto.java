86c4013777014c5d754c4b9e616677f8b4504aca
noeyoes <>
2025-05-09 10:41:43 +0900

    feat: 가맹점주 회원가입 기능 구현

--- src/main/java/com/example/Tokkit_server/merchant/dto/request/CreateMerchantRequestDto.java
+++ src/main/java/com/example/Tokkit_server/merchant/dto/request/CreateMerchantRequestDto.java
@@ -1,13 +1,21 @@
 package com.example.Tokkit_server.merchant.dto.request;
 
-import com.example.Tokkit_server.global.entity.StoreCategory;
 import com.example.Tokkit_server.merchant.entity.Merchant;
-import lombok.Getter;
+import com.example.Tokkit_server.region.entity.Region;
+import com.example.Tokkit_server.store.entity.Store;
+import com.example.Tokkit_server.store.enums.StoreCategory;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Point;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateMerchantRequestDto {
 
+    // 가맹점주 정보
     private String name;
     private String email;
     private String phoneNumber;
@@ -15,22 +23,43 @@ public class CreateMerchantRequestDto {
     private String simplePassword;
     private String businessNumber;
 
+    // 가맹점 정보
     private String storeName;
-    private String storeAddress;
-    protected StoreCategory storeCategory;
+    private String roadAddress;
+    private String newZipcode;
 
-    public Merchant toEntity(PasswordEncoder passwordEncoder) {
-        String encodedPassword = passwordEncoder.encode(password);
-        String encodedSimplePassword = passwordEncoder.encode(simplePassword);
+    private String sidoName;
+    private String sigunguName;
 
+    private StoreCategory storeCategory;
+
+    // Merchant 엔티티 생성
+    public Merchant toMerchantEntity(PasswordEncoder encoder) {
         return Merchant.builder()
                 .name(name)
                 .email(email)
                 .phoneNumber(phoneNumber)
-                .password(encodedPassword)
-                .simplePassword(encodedSimplePassword)
+                .password(encoder.encode(password))
+                .simplePassword(encoder.encode(simplePassword))
                 .businessNumber(businessNumber)
                 .isDormant(false)
+                .roles("MERCHANT")
+                .build();
+    }
+
+    // Store 엔티티 생성
+    public Store toStoreEntity(Merchant merchant, Region region, StoreCategory category,
+                               double latitude, double longitude, Point point) {
+        return Store.builder()
+                .storeName(storeName)
+                .roadAddress(roadAddress)
+                .newZipcode(newZipcode)
+                .latitude(latitude)
+                .longitude(longitude)
+                .location(point)
+                .merchant(merchant)
+                .region(region)
+                .storeCategory(category)
                 .build();
     }
 }
