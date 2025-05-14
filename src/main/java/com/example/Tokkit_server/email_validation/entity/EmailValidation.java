ec74a073a84505e132cb59a852ac4810dfd79db0
YoungjaeRo <ryj0330@naver.com>
2025-05-14 13:48:56 +0900

    refactor : 추가된 클래스 추가

--- src/main/java/com/example/Tokkit_server/email_validation/entity/EmailValidation.java
+++ src/main/java/com/example/Tokkit_server/email_validation/entity/EmailValidation.java
@@ -1,6 +1,6 @@
 package com.example.Tokkit_server.email_validation.entity;
 
-import com.example.Tokkit_server.email_validation.enums.EmailPurpose;
+
 import com.example.Tokkit_server.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -14,12 +14,7 @@ import java.util.Date;
 @Builder
 public class EmailValidation extends BaseTimeEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     private String email;
-
-    @Enumerated(EnumType.STRING)
-    private EmailPurpose purpose;
 
     private String ePw;
     private Date exp;
--- src/main/java/com/example/Tokkit_server/store/enums/StoreCategory.java
+++ src/main/java/com/example/Tokkit_server/store/enums/StoreCategory.java
@@ -1,10 +1,23 @@
 package com.example.Tokkit_server.store.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum StoreCategory {
-    FOOD,
-    MEDICAL,
-    SERVICE,
-    TOURISM,
-    LODGING,
-    EDUCATION
+    FOOD("음식점"),
+    MEDICAL("의료"),
+    SERVICE("서비스"),
+    TOURISM("관광"),
+    LODGING("숙박"),
+    EDUCATION("교육");
+
+    private final String koreanName;
+
+    StoreCategory(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    @JsonValue
+    public String getKoreanName() {
+        return koreanName;
+    }
 } 
