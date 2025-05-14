c99c50a3f00c0eb37da162bd84d2746722d8c7a9
sengjun0624 <sengjun0624@naver.com>
2025-05-14 09:04:23 +0900

    :sparkles: feat: StoreCategory 한글 이름 필드 추가

--- src/main/java/com/example/Tokkit_server/store/enums/StoreCategory.java
+++ src/main/java/com/example/Tokkit_server/store/enums/StoreCategory.java
@@ -1,11 +1,25 @@
 package com.example.Tokkit_server.store.enums;
 
-public enum StoreCategory {
-    FOOD,
-    MEDICAL,
-    SERVICE,
-    TOURISM,
-    LODGING,
-    EDUCATION
-}
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
 
+@Getter
+public enum StoreCategory {
+    FOOD("음식점"),
+    MEDICAL("의료"),
+    SERVICE("서비스"),
+    TOURISM("관광"),
+    LODGING("숙박"),
+    EDUCATION("교육");
+
+    private final String koreanName;
+
+    StoreCategory(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    @JsonValue
+    public String getKoreanName() {
+        return koreanName;
+    }
+}
