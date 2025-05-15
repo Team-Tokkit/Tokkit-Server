7b75e99f6ef0f36379e5fad484b48afd0e293a70
sengjun0624 <sengjun0624@naver.com>
2025-05-15 10:44:11 +0900

    :sparkles: feat: add DTOs and repository

--- src/main/java/com/example/Tokkit_server/region/repository/RegionRepository.java
+++ src/main/java/com/example/Tokkit_server/region/repository/RegionRepository.java
@@ -3,5 +3,9 @@ package com.example.Tokkit_server.region.repository;
 import com.example.Tokkit_server.region.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RegionRepository extends JpaRepository<Region, Long> {
+    List<Region> findDistinctBySidoNameNotNull(); // 중복 제거된 시/도 이름 조회
+    List<Region> findBySidoName(String sidoName); // 해당 시/도의 시군구 조회
 }
