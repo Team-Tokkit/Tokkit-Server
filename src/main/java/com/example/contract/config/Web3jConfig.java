2f510bf3511cd416e85e4c576d3aa38b5e557140
YoungjaeRo <ryj0330@naver.com>
2025-05-26 22:11:18 +0900

    refactor : 통신으로 스마트컨트랙트 받아오는 로직으로 변경후 설정 변경

--- src/main/java/com/example/contract/config/Web3jConfig.java
+++ src/main/java/com/example/contract/config/Web3jConfig.java
@@ -1,6 +1,7 @@
 package com.example.contract.config;
 
-import lombok.RequiredArgsConstructor;
+import java.math.BigInteger;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.web3j.crypto.Credentials;
@@ -9,35 +10,26 @@ import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.gas.ContractGasProvider;
 import org.web3j.tx.gas.StaticGasProvider;
 
-import java.math.BigInteger;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class Web3jConfig {
 
-    private final ContractAddressLoader contractAddressLoader;
-
     @Bean
     public Web3j web3j() {
-        // 로컬 Docker Hardhat 노드 주소
         return Web3j.build(new HttpService("http://localhost:8545"));
     }
 
     @Bean
     public Credentials credentials() {
-        // Account #0의 공개된 프라이빗 키
         return Credentials.create("ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
     }
 
     @Bean
     public ContractGasProvider gasProvider() {
-        BigInteger gasPrice = BigInteger.valueOf(20_000_000_000L); // 20 Gwei
-        BigInteger gasLimit = BigInteger.valueOf(6_700_000);       // 기본 가스 한도
+        BigInteger gasPrice = BigInteger.valueOf(20_000_000_000L);
+        BigInteger gasLimit = BigInteger.valueOf(6_700_000);
         return new StaticGasProvider(gasPrice, gasLimit);
-    }
-
-    @Bean
-    public String tokkitTokenAddress() {
-        return contractAddressLoader.getContractAddress();
     }
 }
