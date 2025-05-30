2e9b748e26911324cedd5846bba11a5762a10082
YoungjaeRo <ryj0330@naver.com>
2025-05-30 21:15:39 +0900

    feat : 자동 전환 응답 DTO 구현

---
+++ src/main/java/com/example/Tokkit_server/wallet/dto/response/AutoConvertSettingResponse.java
@@ -0,0 +1,15 @@
+package com.example.Tokkit_server.wallet.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AutoConvertSettingResponse {
+    private boolean enabled;
+    private int dayOfMonth;
+    private int hour;
+    private long amount;
+}
